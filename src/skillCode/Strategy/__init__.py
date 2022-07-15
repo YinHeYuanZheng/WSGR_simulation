6fbc6e2514094012ab8e5683ca1122d8fa33829b
YinHeYuanZheng <1123647509@qq.com>
2022-07-15 20:41:20 +0800

    分类装备技能和战术

---
+++ src/skillCode/Strategy/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# Author:银河远征
+# env:py38
+# 战术效果
+
+from . import stid131  # 大角度规避
+from . import stid233  # 后备弹
--- src/skillCode/__init__.py
+++ src/skillCode/__init__.py
@@ -2,22 +2,8 @@
 # Author:银河远征
 # env:py38
 
-# 装备特效
-from . import esid003  # 装备穿甲特效
-from . import esid004  # 超重弹特效
-from . import esid005  # 高脚柜特效
-from . import esid012  # 烈风改J特效
-from . import esid016  # 潜载火箭炮特效
-from . import esid018  # 远程对空警戒雷达
-from . import esid019  # 袖珍舰载艇
-from . import esid020  # 海鹰导弹发射器
-from . import esid021  # 海鹰反舰导弹
-from . import esid022  # 三坐标搜索雷达
-from . import esid023  # 暴击率特效
-
-# 战术效果
-from . import stid131  # 大角度规避
-from . import stid233  # 后备弹
+from .Equipment import *  # 装备特效
+from .Strategy import *  # 战术效果
 
 # 未改航系
 from . import sid101191  # 皇家方舟-1
