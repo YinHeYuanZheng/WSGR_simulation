f264eeecf7270c4c37ab4d8bba86130226e47735
YinHeYuanZheng <1123647509@qq.com>
2023-03-25 20:34:36 +0800

    更新模拟数据和批量化接口

--- main.py
+++ main.py
@@ -18,17 +18,26 @@ from src.wsgr.wsgrTimer import timer
 from src.wsgr.formulas import *
 
 
-if __name__ == '__main__':
-    configDir = os.path.join(os.path.dirname(srcDir), 'config')
-    xml_file = os.path.join(configDir, r'event\event04\config_3.xml')
-    dependDir = os.path.join(os.path.dirname(srcDir), 'depend')
-    data_file = os.path.join(dependDir, r'ship\database.xlsx')
-    ds = Dataset(data_file)  # 舰船数据
-
-    mapDir = os.path.join(dependDir, r'map')
+def main(xml_file, epoc, fun):
     timer_init = timer()  # 创建时钟
     battle = load_config(xml_file, mapDir, ds, timer_init)
-    del ds
-
     set_supply(battle, 4)
-    run_victory(battle, 5000)
+    fun(battle, epoc)
+
+
+configDir = os.path.join(os.path.dirname(srcDir), 'config')
+dependDir = os.path.join(os.path.dirname(srcDir), 'depend')
+data_file = os.path.join(dependDir, r'ship\database.xlsx')
+mapDir = os.path.join(dependDir, r'map')
+ds = Dataset(data_file)  # 舰船数据
+
+if __name__ == '__main__':
+    epoc = 3000
+    fun = run_victory
+    # xml_file = os.path.join(configDir, r'event\event04\config_10.xml')
+    # main(xml_file, epoc, fun)
+
+    for num in [2, 3, 4, 7, 10]:
+        xml_file = os.path.join(configDir, rf'event\event04\config_{num}.xml')
+        main(xml_file, epoc, fun)
+        print('\n')
