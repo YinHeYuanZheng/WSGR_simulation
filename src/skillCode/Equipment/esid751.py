d6aeec1f2e09c47dd27ad6894a00608f761170db
YinHeYuanZheng <1123647509@qq.com>
2026-03-08 00:41:56 +0800

    新装备技能和数据

--- src/skillCode/Equipment/esid751.py
+++ src/skillCode/Equipment/esid751.py
@@ -1,15 +1,15 @@
 # -*- coding:utf-8 -*-
 # Author:银河远征
 # env:py38
-# П-35反舰导弹
+# “白蛉”反舰导弹
 
 from src.wsgr.skill import *
 from src.wsgr.ship import *
 from src.wsgr.phase import *
 
 
-class Eskill_612(EquipSkill):
-    """增加10%护甲穿透(同类弹药效果只生效一个)，格罗兹尼装备时额外提高5%护甲穿透"""
+class Eskill_751(EquipSkill):
+    """增加15%护甲穿透(同类弹药效果只生效一个)，S国舰船装备时战斗中额外增加5%护甲穿透"""
     def __init__(self, timer, master, value):
         super().__init__(timer, master, value)
         self.target = SelfTarget(master)
@@ -19,15 +19,15 @@ class Eskill_612(EquipSkill):
                 effect_type=3,
                 name='pierce_coef',
                 phase=AllPhase,
-                value=0.1,
+                value=0.15,
                 bias_or_weight=0
             )
         ]
-        if master.cid in ['10533', '11533']:
+        if master.status['country'] == 'S':
             self.buff.append(
                 EquipEffect(
                     timer=timer,
-                    effect_type=612,
+                    effect_type=751,
                     name='pierce_coef',
                     phase=AllPhase,
                     value=0.05,
@@ -36,4 +36,4 @@ class Eskill_612(EquipSkill):
             )
 
 
-skill = [Eskill_612]
+skill = [Eskill_751]
